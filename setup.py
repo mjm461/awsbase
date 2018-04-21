989dd4a4f5a5011c4f5ffd8bf23230849f13acff
Mark McClain <mjm461@gmail.com>
2018-04-21 09:24:34 -0400

    add license and simplify setup

--- setup.py
+++ setup.py
@@ -1,37 +1,23 @@
-#!/usr/bin/env python
-import os
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-VERSION = open(os.path.join(here, 'VERSION')).read().strip()
 
-install_requires = [
-    # 'boto3' # install on own locally (pip instlal boto3) - lambda's containers already include this
-]
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
 
 setup(
-    name='awsbase',
-    version=VERSION,
-    description="",
-    author="mark mcclain",
+    name='awwsbase',
+    version='0.0.1',
+    description='Common AWS code',
+    long_description=readme,
+    author='Mark McClain',
     author_email='mjm461@gmail.com',
-    url='',
+    url='https://github.com/mjm461/awsbase',
+    license=license,
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    namespace_packages=['awsbase'],
-    package_data={'awsbase': ['static/*.*', 'templates/*.*']},
-    install_requires=install_requires,
-    extras_require=dict(
-        test=install_requires + [
-            'pytest>=3.5',
-        ],
-        develop=install_requires + [
-            'ipdb>=0.11',
-        ]),
-    zip_safe=False,
-    entry_points={
-        'console_scripts': [
-        ],
-    },
-    dependency_links=[
-    ],
+    install_requires = [
+        #'boto3' # AWS containers already include the latest version - add manually if you want it
+    ]
 )
